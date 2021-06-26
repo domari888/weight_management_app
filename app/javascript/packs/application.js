d0d4894873680219e522ad957efe3ad73e9d829c
damari <motokin128@gmail.com>
2021-06-26 10:25:42 +0900

    各種ライブラリのテスト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import { Chart } from "chart.js"
+// import { Chart } from "chart.js"
+import 'chart.js/dist/chart';
 
 
 Rails.start()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,8 @@
 const { environment } = require('@rails/webpacker')
 // jQuery を Bootstrap 以外でも使用する
 const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProgressPlugin({
-    $: 'jquery',
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
   jQuery: 'jquery',
   Popper: ['popper.js', 'default']
 }))
