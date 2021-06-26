86a6817282163c1386036cfdeff14a012eabe8ca
damari <motokin128@gmail.com>
2021-06-26 23:37:42 +0900

    モーダルを作成
    新規投稿フォームを実装するため

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'graphs#index'
   devise_for :users
-  resourse :graphs, only: %i[index create update]
+  resource :graphs, only: %i[index create update]
 end
