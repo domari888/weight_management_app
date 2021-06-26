e65e06844d04707e75f9d12d631aef2e2770c2e9
damari <motokin128@gmail.com>
2021-06-26 16:17:29 +0900

    Graph モデルを作成して、バリデーションを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_26_012811) do
+ActiveRecord::Schema.define(version: 2021_06_26_070926) do
+
+  create_table "graphs", charset: "utf8mb4", force: :cascade do |t|
+    t.date "date", null: false
+    t.float "weight", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "date"], name: "index_graphs_on_user_id_and_date", unique: true
+    t.index ["user_id"], name: "index_graphs_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_06_26_012811) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "graphs", "users"
 end
